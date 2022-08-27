cd949109db28fe06c2f026b49e9a98e02ab34af2
rong <1127080666@qq.com>
2022-08-27 09:05:50 +0800

    update

--- web/src/views/portal/index.vue
+++ web/src/views/portal/index.vue
@@ -113,234 +113,234 @@
 </template>
 
 <script>
-import * as allApi from '../../../api.js'
-export default {
-  data() {
-    const item_1 = [
-      {
-        id: '1',
-        IPAddress: '192.168.2.105',
-        userGroup: '无线组——华为手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 20:27:30',
-        phoneNumber: '17813564375',
-      },
-      {
-        id: '2',
-        IPAddress: '192.168.1.108',
-        userGroup: '无线组——荣耀手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 19:16:07',
-        phoneNumber: '15673468345',
-      },
-      {
-        id: '3',
-        IPAddress: '192.168.2.35',
-        userGroup: '无线组——iPhone',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 19:03:52',
-        phoneNumber: '18944274542',
-      },
-      {
-        id: '4',
-        IPAddress: '192.168.3.141',
-        userGroup: '无线组——华为手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 18:55:48',
-        phoneNumber: '16746557564',
-      },
-      {
-        id: '5',
-        IPAddress: '192.168.1.33',
-        userGroup: '无线组——荣耀手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 18:47:03',
-        phoneNumber: '17867465649',
-      },
-      {
-        id: '6',
-        IPAddress: '192.168.3.76',
-        userGroup: '无线组——VIVO手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 18:44:27',
-        phoneNumber: '18768656355',
-      },
-      {
-        id: '7',
-        IPAddress: '192.168.3.9',
-        userGroup: '无线组——小米手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 18:42:11',
-        phoneNumber: '17685324543',
-      },
-      {
-        id: '8',
-        IPAddress: '192.168.2.104',
-        userGroup: '无线组——OPPO手机',
-        ifPortal: '已认证',
-        visitTime: '2022-07-02 18:31:09',
-        phoneNumber: '19878456324',
-      },
-    ]
-    const item_2 = [
-      {
-        id: '1',
-        IPAddress: '192.168.2.45',
-        userGroup: '无线组——iPhone',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 21:15:58',
-      },
-      {
-        id: '2',
-        IPAddress: '192.168.3.103',
-        userGroup: '无线组——OPPO手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 19:56:03',
-      },
-      {
-        id: '3',
-        IPAddress: '192.168.1.48',
-        userGroup: '无线组——iPhone',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 19:48:01',
-      },
-      {
-        id: '4',
-        IPAddress: '192.168.2.61',
-        userGroup: '无线组——华为手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 18:37:27',
-      },
-      {
-        id: '5',
-        IPAddress: '192.168.3.84',
-        userGroup: '无线组——VIVO手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 18:20:18',
-      },
-      {
-        id: '6',
-        IPAddress: '192.168.1.15',
-        userGroup: '无线组——华为手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 18:15:29',
-      },
-      {
-        id: '7',
-        IPAddress: '192.168.2.17',
-        userGroup: '无线组——小米手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 18:13:52',
-      },
-      {
-        id: '8',
-        IPAddress: '192.168.1.88',
-        userGroup: '无线组——华为手机',
-        ifPortal: '未认证',
-        visitTime: '2022-07-02 18:11:35',
-      },
-    ]
+  import * as allApi from '../../../api.js'
+  export default {
+    data() {
+      const item_1 = [
+        {
+          id: '1',
+          IPAddress: '192.168.2.105',
+          userGroup: '无线组——华为手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 20:27:30',
+          phoneNumber: '17813564375',
+        },
+        {
+          id: '2',
+          IPAddress: '192.168.1.108',
+          userGroup: '无线组——荣耀手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 19:16:07',
+          phoneNumber: '15673468345',
+        },
+        {
+          id: '3',
+          IPAddress: '192.168.2.35',
+          userGroup: '无线组——iPhone',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 19:03:52',
+          phoneNumber: '18944274542',
+        },
+        {
+          id: '4',
+          IPAddress: '192.168.3.141',
+          userGroup: '无线组——华为手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 18:55:48',
+          phoneNumber: '16746557564',
+        },
+        {
+          id: '5',
+          IPAddress: '192.168.1.33',
+          userGroup: '无线组——荣耀手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 18:47:03',
+          phoneNumber: '17867465649',
+        },
+        {
+          id: '6',
+          IPAddress: '192.168.3.76',
+          userGroup: '无线组——VIVO手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 18:44:27',
+          phoneNumber: '18768656355',
+        },
+        {
+          id: '7',
+          IPAddress: '192.168.3.9',
+          userGroup: '无线组——小米手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 18:42:11',
+          phoneNumber: '17685324543',
+        },
+        {
+          id: '8',
+          IPAddress: '192.168.2.104',
+          userGroup: '无线组——OPPO手机',
+          ifPortal: '已认证',
+          visitTime: '2022-07-02 18:31:09',
+          phoneNumber: '19878456324',
+        },
+      ]
+      const item_2 = [
+        {
+          id: '1',
+          IPAddress: '192.168.2.45',
+          userGroup: '无线组——iPhone',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 21:15:58',
+        },
+        {
+          id: '2',
+          IPAddress: '192.168.3.103',
+          userGroup: '无线组——OPPO手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 19:56:03',
+        },
+        {
+          id: '3',
+          IPAddress: '192.168.1.48',
+          userGroup: '无线组——iPhone',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 19:48:01',
+        },
+        {
+          id: '4',
+          IPAddress: '192.168.2.61',
+          userGroup: '无线组——华为手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 18:37:27',
+        },
+        {
+          id: '5',
+          IPAddress: '192.168.3.84',
+          userGroup: '无线组——VIVO手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 18:20:18',
+        },
+        {
+          id: '6',
+          IPAddress: '192.168.1.15',
+          userGroup: '无线组——华为手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 18:15:29',
+        },
+        {
+          id: '7',
+          IPAddress: '192.168.2.17',
+          userGroup: '无线组——小米手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 18:13:52',
+        },
+        {
+          id: '8',
+          IPAddress: '192.168.1.88',
+          userGroup: '无线组——华为手机',
+          ifPortal: '未认证',
+          visitTime: '2022-07-02 18:11:35',
+        },
+      ]
 
-    return {
-      tableData_1: item_1,
-      tableData_2: item_2,
-      activeName: 'first',
-    }
-  },
-  methods: {
-    async getData() {
-      let list = []
-      var jsonObj = await allApi.getUnportalTraveller()
-      for (var i = 0; i < jsonObj.length && list.length < 50; i++) {
-        if (jsonObj[i].humanFlow < 50000) list.push(jsonObj[i].humanFlow)
+      return {
+        tableData_1: item_1,
+        tableData_2: item_2,
+        activeName: 'first',
       }
-      return list
     },
-  },
-}
+    methods: {
+      async getData() {
+        let list = []
+        var jsonObj = await allApi.getUnportalTraveller()
+        for (var i = 0; i < jsonObj.length && list.length < 50; i++) {
+          if (jsonObj[i].humanFlow < 50000) list.push(jsonObj[i].humanFlow)
+        }
+        return list
+      },
+    },
+  }
 </script>
 
 <style>
-#whole {
-  font-family: 'Georgia', 'Lucida Sans', '微软雅黑', 'sans-serif',
-    'Lucida Grande', 'Lucida Sans';
-  margin-top: 30px;
-}
-.warning {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  transition: all 0.15s ease;
-  display: inline-block;
-  line-height: 1;
-  white-space: nowrap;
-  cursor: pointer;
-  background: #f3b2b2;
-  color: rgb(0, 0, 0) ed;
-  -webkit-appearance: none;
-  text-align: center;
-  box-sizing: border-box;
-  outline: none;
-  margin: 0;
-  font-weight: 500;
-  /* -moz-user-select: none; */
-  /*    -webkit-user-select: none; */
-  /* -ms-user-select: none; */
-  padding: 12px 30px;
-  font-size: 16px;
-  border-radius: 50px;
-}
-.disinfect {
-  margin-left: 80px;
-  margin-top: 50px;
-  margin-bottom: 20px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  transition: all 0.15s ease;
-  display: inline-block;
-  line-height: 1;
-  white-space: nowrap;
-  cursor: pointer;
-  -webkit-appearance: none;
-  text-align: center;
-  box-sizing: border-box;
-  outline: none;
-  font-weight: 500;
-  padding: 1% 2% 2.5%;
-  font-size: 22px;
-  border-radius: 20px;
-}
-.open {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  transition: all 0.15s ease;
-  display: inline-block;
-  line-height: 1;
-  white-space: nowrap;
-  cursor: pointer;
-  background: #e6a23c;
-  color: #ededed;
-  -webkit-appearance: none;
-  text-align: center;
-  box-sizing: border-box;
-  outline: none;
-  margin: 0;
-  font-weight: 500;
-  /* -moz-user-select: none; */
-  /* -webkit-user-select: none; */
-  /* -ms-user-select: none; */
-  padding: 12px 30px;
-  font-size: 16px;
-  border-radius: 50px;
-}
-.el-header {
-  background-color: #b3c0d1;
-  line-height: 60px;
-}
-.cot {
-  border-radius: 10px 10px 10px 10px;
-  border-top: 1px solid #dedede;
-  border-left: 1px solid #dedede;
-  border-bottom: 1px solid #dedede;
-  width: 90%;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  transition: all 0.15s ease;
-  border: 1px solid #eee;
-}
+  #whole {
+    font-family: 'Georgia', 'Lucida Sans', '微软雅黑', 'sans-serif',
+      'Lucida Grande', 'Lucida Sans';
+    margin-top: 30px;
+  }
+  .warning {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transition: all 0.15s ease;
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background: #f3b2b2;
+    color: rgb(0, 0, 0) ed;
+    -webkit-appearance: none;
+    text-align: center;
+    box-sizing: border-box;
+    outline: none;
+    margin: 0;
+    font-weight: 500;
+    /* -moz-user-select: none; */
+    /*    -webkit-user-select: none; */
+    /* -ms-user-select: none; */
+    padding: 12px 30px;
+    font-size: 16px;
+    border-radius: 50px;
+  }
+  .disinfect {
+    margin-left: 80px;
+    margin-top: 50px;
+    margin-bottom: 20px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transition: all 0.15s ease;
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    -webkit-appearance: none;
+    text-align: center;
+    box-sizing: border-box;
+    outline: none;
+    font-weight: 500;
+    padding: 1% 2% 2.5%;
+    font-size: 22px;
+    border-radius: 20px;
+  }
+  .open {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transition: all 0.15s ease;
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background: #e6a23c;
+    color: #ededed;
+    -webkit-appearance: none;
+    text-align: center;
+    box-sizing: border-box;
+    outline: none;
+    margin: 0;
+    font-weight: 500;
+    /* -moz-user-select: none; */
+    /* -webkit-user-select: none; */
+    /* -ms-user-select: none; */
+    padding: 12px 30px;
+    font-size: 16px;
+    border-radius: 50px;
+  }
+  .el-header {
+    background-color: #b3c0d1;
+    line-height: 60px;
+  }
+  .cot {
+    border-radius: 10px 10px 10px 10px;
+    border-top: 1px solid #dedede;
+    border-left: 1px solid #dedede;
+    border-bottom: 1px solid #dedede;
+    width: 90%;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transition: all 0.15s ease;
+    border: 1px solid #eee;
+  }
 </style>
